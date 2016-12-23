787f02e311d503cd5bb9d5b29b65742940392b09
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2016-12-23 09:01:46 -0200

    Do not use babel-polyfill, and do not try to check for fetch polyfill either
    use react-native-babel-preset-0 instead of babel-preset-0 and babel-preset-es2015
    fix #5

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
-import 'babel-polyfill';
-
-const fetch = typeof window === 'object' ? window.fetch : global.fetch;
-
-if (!fetch)
-  throw new Error(
-    'A global `fetch` method is required as either `window.fetch` '
-    + 'for browsers or `global.fetch` for node runtime environments. '
-    + 'Please add `require(\'isomorphic-fetch\')` before importing `frisbee`. '
-    + 'You may optionally `require(\'es6-promise\').polyfill()` before you '
-    + 'require `isomorphic-fetch` if you want to support older browsers.'
-    + '\n\nFor more info: https://github.com/niftylettuce/frisbee#usage'
-  );
-
 const productionHost = 'buy.itunes.apple.com';
 const sandboxHost = 'sandbox.itunes.apple.com';
 
 const statusCodes = {
-  [0]:     { message:'Active', valid: true, error: false },
-  [21000]: { message:'App store could not read', valid: false, error: true },
-  [21002]: { message:'Data was malformed', valid: false, error: true },
-  [21003]: { message:'Receipt not authenticated', valid: false, error: true },
-  [21004]: { message:'Shared secret does not match', valid: false, error: true },
-  [21005]: { message:'Receipt server unavailable', valid: false, error: true },
-  [21006]: { message:'Receipt valid but sub expired', valid: false, error: false },
-  [21007]: { message:'Sandbox receipt sent to Production environment', valid: false, error: true, redirect: true }, // special case for app review handling - forward any request that is intended for the Sandbox but was sent to Production, this is what the app review team does
-  [21008]: { message:'Production receipt sent to Sandbox environment', valid: false, error: true },
+  [0]:     { message: 'Active', valid: true, error: false },
+  [21000]: { message: 'App store could not read', valid: false, error: true },
+  [21002]: { message: 'Data was malformed', valid: false, error: true },
+  [21003]: { message: 'Receipt not authenticated', valid: false, error: true },
+  [21004]: { message: 'Shared secret does not match', valid: false, error: true },
+  [21005]: { message: 'Receipt server unavailable', valid: false, error: true },
+  [21006]: { message: 'Receipt valid but sub expired', valid: false, error: false },
+  /**
+   * special case for app review handling - forward any request that is intended for the Sandbox but was sent to
+   * Production, this is what the app review team does
+   */
+  [21007]: { message: 'Sandbox receipt sent to Production environment', valid: false, error: true, redirect: true },
+  [21008]: { message: 'Production receipt sent to Sandbox environment', valid: false, error: true },
 };
 
 function VerificationError(error) {
@@ -32,7 +22,7 @@ function VerificationError(error) {
 }
 VerificationError.prototype = Object.create(Error.prototype);
 
-export default function(password, production=true) {
+export default function (password, production = true) {
   const endpoint = production ? productionHost : sandboxHost;
   const verifyUrl = `https://${endpoint}/verifyReceipt`;
 
@@ -58,6 +48,6 @@ export default function(password, production=true) {
     }
 
     return body;
-  }
+  };
 }
 
--- test/index.js
+++
@@ -1,8 +0,0 @@
-import { expect } from 'chai';
-import app from '../src';
-
-describe('sanity', () => {
-  it('should exist', () => {
-    expect('it').be.equal(true);
-  });
-});
